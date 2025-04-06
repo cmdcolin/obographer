ef237427a901c52792132e5e705787924eaa4ce6
Colin <colin.diesh@gmail.com>
2025-04-06 16:21:05 -0500

    Updates

--- README.md
+++ README.md
@@ -1,62 +1,12 @@
-# React + TypeScript + Vite
+# obographer
 
-This template provides a minimal setup to get React working in Vite with HMR and
-some ESLint rules.
+Plots both parent and child elements of a GO ID, reading directly from a gene
+ontology "obograph" JSON file
 
-Currently, two official plugins are available:
+## Demo
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md)
-  uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc)
-  uses [SWC](https://swc.rs/) for Fast Refresh
+https://cmdcolin.github.io/obographer
 
-## Expanding the ESLint configuration
+## Footnote
 
-If you are developing a production application, we recommend updating the
-configuration to enable type-aware lint rules:
-
-```js
-export default tseslint.config({
-  extends: [
-    // Remove ...tseslint.configs.recommended and replace with this
-    ...tseslint.configs.recommendedTypeChecked,
-    // Alternatively, use this for stricter rules
-    ...tseslint.configs.strictTypeChecked,
-    // Optionally, add this for stylistic rules
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-You can also install
-[eslint-plugin-react-x](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-x)
-and
-[eslint-plugin-react-dom](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-dom)
-for React-specific lint rules:
-
-```js
-// eslint.config.js
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-
-export default tseslint.config({
-  plugins: {
-    // Add the react-x and react-dom plugins
-    'react-x': reactX,
-    'react-dom': reactDom,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended typescript rules
-    ...reactX.configs['recommended-typescript'].rules,
-    ...reactDom.configs.recommended.rules,
-  },
-})
-```
+Made at Biocuration 2025
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import CytoscapeComponent from 'react-cytoscapejs'
 import cytoscape from 'cytoscape'
 import dagre from 'cytoscape-dagre'
+import COSEBilkent from 'cytoscape-cose-bilkent'
+
+cytoscape.use(COSEBilkent)
 
 cytoscape.use(dagre)
 
+interface Graph {
+  nodes: { id: string; lbl: string }[]
+  edges: { sub: string; obj: string }[]
+}
+
 // Custom hook to fetch the GO ontology file
 function useGoOntology() {
-  const [ontology, setOntology] = useState()
+  const [ontology, setOntology] = useState<{
+    graphs: Graph[]
+  }>()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState<unknown>()
 
@@ -37,20 +47,122 @@ function useGoOntology() {
   return { ontology, loading, error }
 }
 
+function idMaker(r: string) {
+  return r.replace('http://purl.obolibrary.org/obo/', '').replace('_', ':')
+}
+
 function App() {
-  const [val, setVal] = useState('')
+  const [val, setVal] = useState('GO:0045010')
   const { ontology, loading, error } = useGoOntology()
 
   console.log({ ontology })
-  // const r = ontology.graph.nodes.map(r => {})
-  // Default elements for the graph
-  const elements = [
-    { data: { id: 'one', label: 'Node 1' } },
-    { data: { id: 'two', label: 'Node 2' } },
-    {
-      data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' },
-    },
-  ]
+
+  const nodes = useMemo(() => {
+    if (ontology) {
+      const nodes = Object.fromEntries(
+        ontology.graphs[0].nodes.map(r => {
+          const id = idMaker(r.id)
+          return [
+            id,
+            {
+              ...r,
+              id,
+              parents: [] as string[],
+              children: [] as string[],
+            },
+          ]
+        }),
+      )
+      for (const edge of ontology.graphs[0].edges) {
+        nodes[idMaker(edge.sub)]?.parents.push(idMaker(edge.obj))
+        nodes[idMaker(edge.obj)]?.children.push(idMaker(edge.sub))
+      }
+      return nodes
+    } else {
+      return undefined
+    }
+  }, [ontology])
+  const m = useMemo(() => {
+    if (nodes) {
+      const currentNode = nodes[val]
+      if (!currentNode) {
+        return undefined
+      }
+
+      const visited = new Set<string>()
+      const visited2 = new Set<string>()
+      const resultNodes: { label: string; id: string }[] = []
+      const resultEdges: { source: string; target: string; label: string }[] =
+        []
+
+      function traverseParents(nodeId: string) {
+        if (visited.has(nodeId)) {
+          return
+        }
+
+        visited.add(nodeId)
+        resultNodes.push({
+          id: nodeId,
+          label: nodes?.[nodeId].lbl,
+        })
+
+        const node = nodes![nodeId]
+        if (node?.parents) {
+          for (const parentId of node.parents) {
+            const parentNodeId = idMaker(parentId)
+            resultEdges.push({
+              source: nodeId,
+              target: parentId,
+              label: '',
+            })
+            traverseParents(parentNodeId)
+          }
+        }
+      }
+
+      function traverseChildren(nodeId: string) {
+        if (visited2.has(nodeId)) {
+          return
+        }
+
+        visited2.add(nodeId)
+        resultNodes.push({
+          id: nodeId,
+          label: nodes![nodeId].lbl,
+        })
+
+        const node = nodes![nodeId]
+        if (node?.children) {
+          for (const childId of node.children) {
+            const childNodeId = idMaker(childId)
+            resultEdges.push({
+              source: childId,
+              target: nodeId,
+              label: '',
+              color: 'red',
+            })
+            traverseChildren(childNodeId)
+          }
+        }
+      }
+
+      traverseParents(val)
+      traverseChildren(val)
+      return {
+        resultNodes,
+        resultEdges,
+      }
+    } else {
+      return undefined
+    }
+  }, [nodes, val])
+
+  const elements = m
+    ? [
+        ...m.resultNodes.map(m => ({ data: m })),
+        ...m.resultEdges.map(m => ({ data: m })),
+      ]
+    : undefined
 
   return (
     <div>
@@ -61,19 +173,25 @@ function App() {
           onChange={event => setVal(event.target.value)}
         />
         <div>
-          {loading && <p>Loading GO ontology...</p>}
+          {loading ? <p>Loading GO ontology...</p> : null}
           {error ? <p style={{ color: 'red' }}>Error: {`${error}`}</p> : null}
           {ontology ? <p>GO ontology loaded successfully!</p> : null}
         </div>
       </div>
-      <CytoscapeComponent
-        elements={elements}
-        style={{
-          width: '1800px',
-          height: '1000px',
-        }}
-        layout={{ name: 'dagre' }}
-      />
+      {elements ? (
+        <CytoscapeComponent
+          elements={elements}
+          style={{
+            width: '1800px',
+            height: '1000px',
+          }}
+          layout={{
+            name: 'dagre',
+            avoidOverlap: true,
+            nodeDimensionsIncludeLabels: true,
+          }}
+        />
+      ) : null}
     </div>
   )
 }
